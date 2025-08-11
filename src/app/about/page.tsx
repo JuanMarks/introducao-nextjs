43552f0d20024fb0177b59a7508b045f8903d588
JuanMarks <juandev77@gmail.com>
2025-08-11 18:58:05 -0300

    atualizando

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,10 @@
 // import NavBar from "@/components/NavBar"
-import Button from "@/components/Button"
+
 export default function About(){
 
     return(
         <div>
             {/* <NavBar label1="Produtos" label2="Adminitradores" label3="Time"/> */}
-            <Button label="Clique aqui" />
             <h1>Sobre nós</h1>
             <p>Informações sobre a empresa ...</p>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,125 +1,52 @@
-'use client';
-
 import { Card } from "@/components/Card";
 import NavBar from "@/components/NavBar";
-import CommentForm from "@/components/CommentForm";
-import CommentList from "@/components/CommentList";
-import Counter from "@/components/Counter";
-import LikeButton from "@/components/LikeButton";
-import React, { useState } from "react";
 // import PlaceForm from "@/components/PlaceForm";
 
-const cardData = [
-    {
-        title: "Renderização Híbrida",
-        description: "SSG, SSR e ISR para otimizar performance e SEO.",
-        imageUrl: "https://example.com/image1.jpg"
-    },
-    {
-        title: "CSS Utility-First",
-        description: "Tailwind CSS para estilização rápida e consistente.",
-        imageUrl: "https://example.com/image2.jpg"
-    },
-    {
-        title: "TypeScript Integrado",
-        description: "Tipagem estática para um código mais seguro.",
-        imageUrl: "https://example.com/image3.jpg"
-    },
-    {
-        title: "Renderização Híbrida",
-        description: "SSG, SSR e ISR para otimizar performance e SEO.",
-        imageUrl: "https://example.com/image1.jpg"
-    },
-    {
-        title: "CSS Utility-First",
-        description: "Tailwind CSS para estilização rápida e consistente.",
-        imageUrl: "https://example.com/image2.jpg"
-    },
-    {
-        title: "TypeScript Integrado",
-        description: "Tipagem estática para um código mais seguro.",
-        imageUrl: "https://example.com/image3.jpg"
-    }
 
-];
 
-type Comment = {
-  author: string;
-  content: string;
-  date: string;
-};
 
 export default function Home() {
-  const [comments, setComments] = useState<Comment[]>([
-    {
-      author: "João Silva",
-      content: "Este é um ótimo artigo! Aprendi muito.",
-      date: "2023-10-01",
-    },
-    {
-      author: "Maria Oliveira",
-      content: "Muito bem explicado, obrigado!",
-      date: "2023-10-02",
-    },
-  ]);
+  return (
+    <div className=" ">
 
-    const handleAddComment = (commentText: string) => {
-        if (commentText.trim() === "") {
-            // Não adiciona comentários vazios
-            return;
-        }
+      <NavBar links={
+        [
+          { label: 'Inicio', href: '/' },
+          { label: 'Sobre Nós', href: '/about' },
+          { label: 'Produtos', href: '/product' }
+        ]
+      } />
 
-        const newComment: Comment = {
-        author: "Usuário Anônimo", // Você pode customizar o autor
-        content: commentText,
-        date: new Date().toLocaleDateString('pt-BR'), // Formata a data para o padrão brasileiro
-        };
+      <section className="bg-white py-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-        // Adiciona o novo comentário à lista existente
-        setComments([...comments, newComment]);
-    };
-    return (
-        <div className=" ">
+        <h2 className="text-3xl font-bold text-center text-gray-900 mb-12">
+          Recursos Principais
+        </h2>
 
-            <NavBar links={
-                [
-                    { label: 'Inicio', href: '/' },
-                    { label: 'Sobre Nós', href: '/about' },
-                    { label: 'Produtos', href: '/product' }
-                ]
-            } />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8" >
+          <Card
+            title="Renderização Híbrida"
+            description="SSG, SSR e ISR para otimizar performance e SEO."
+            icon="🚀" />
 
-            <section className="bg-white py-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Card
+            title="CSS Utility-First"
+            description="Tailwind CSS para estilização rápida e consistente."
+            icon="🎨" />
 
-                <h2 className="text-3xl font-bold text-center text-gray-900 mb-12">
-                    Recursos Principais
-                </h2>
+          <Card
+            title="TypeScript Integrado"
+            description="Tipagem estática para um código mais seguro."
+            icon="🔒" />
+        </div>
 
-                
+      </section>
 
-                <div>
-                    <CommentList comments={comments} />
-                    <CommentForm onSubmit={handleAddComment} />
-                </div>
-
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8" >
-                    {cardData.map((card, index) => (
-                        <Card
-                            key={index}
-                            title={card.title}
-                            description={card.description}
-                            imageUrl={card.imageUrl}
-                        />
-                    ))}
-                </div>
-
-            </section>
-
-            {/* <div className="flex justify-center">
+      {/* <div className="flex justify-center">
         <PlaceForm lat={22} lng={-23}/>
       </div> */}
 
 
-        </div>
-    );
+    </div>
+  );
 }
